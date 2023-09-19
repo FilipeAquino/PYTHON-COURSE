34b2f404c74ddea68f6ac3da569b3a299ed7dc21
FilipeAquino <106785612+FilipeAquino@users.noreply.github.com>
2023-09-19 15:05:56 -0300

    3 - Python's Module

---
+++ Python's Module/main.py
@@ -0,0 +1,10 @@
+# Vamos usar uma tática em python para testar codigos sem atrapalhar os modulos
+
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+
+    main()
+    # serve para executar testes no modulo somente se for diretamente nele
